4644561f4a1430a15836afef63dbbd0691f183eb
Khalil Ahmed Sharif <101020879+khalilahmed97@users.noreply.github.com>
2023-11-30 17:18:05 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,16 @@
 import whisper
 from googletrans import Translator
-import json
 
+# Loading the model and Transcribing the speech into English
 model = whisper.load_model("base")
 result = model.transcribe("test.mp3", language="en", fp16=False)
-json_data = json.dumps(result)
-result_dict = json.loads(json_data)
-print(result_dict["text"])
+print(result["text"])
 
 translator = Translator()
-# Detect the language of the input text (optional)
-# Translate the text to Urdu
 
+# Translate the text to Urdu
 try:
-  translated_text = translator.translate(result_dict["text"], dest='ur')
+  translated_text = translator.translate(result["text"], dest='ur')
   if translated_text is not None:
      print(translated_text.text)
   else:
